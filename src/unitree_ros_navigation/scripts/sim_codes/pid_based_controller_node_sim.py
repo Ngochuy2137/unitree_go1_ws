b8d462bc0f8ec93a32a27f73d0a45c2885f5213d
Ngochuy2137 <huy.nguyenngoc2137@gmail.com>
2025-05-22 17:06:07 +0900

    Add no_cmd param
    Signed-off-by: Ngochuy2137 <huy.nguyenngoc2137@gmail.com>

--- src/unitree_ros_navigation/scripts/sim_codes/pid_based_controller_node_sim.py
+++ src/unitree_ros_navigation/scripts/sim_codes/pid_based_controller_node_sim.py
@@ -220,6 +220,7 @@ class RobotController:
     def __init__(self, robot_ip="192.168.123.161"):
         rospy.init_node('robot_pid_high_level_controller', anonymous=True)
         self.using_real_robot = rospy.get_param("using_real_robot")
+        self.no_cmd = rospy.get_param("~no_cmd", False)
         if self.using_real_robot:
             sys.path.append('/home/server-huynn/workspace/robot_catching_project/experiment/unitree_go1_ws/src/unitree_ros/unitree_ros_to_real/unitree_legged_sdk/lib/python/amd64')
             import robot_interface as sdk
@@ -404,6 +405,9 @@ class RobotController:
         self.udp.Send()
 
     def publish_velocity(self, vx, vy, wz):
+        if self.no_cmd:
+            return
+        
         if self.using_real_robot:
             self.send_udp_message(vx, vy, wz)
         vel_msg = Twist()
